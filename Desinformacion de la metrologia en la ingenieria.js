917270bfd15c049bcefcdc9fff3efdab87280963
jesus1578 <117939338+jesus1578@users.noreply.github.com>
2022-12-15 19:56:52 -0600

    Add files via upload

---
+++ Desinformacion de la metrologia en la ingenieria.js
@@ -0,0 +1,7 @@
+alert("No olvides darle click, para reproducir la música :)");
+
+function changeColor(newColor) {
+    var elem = document.getElementById('Cambio');
+    elem.style.color = newColor;
+ }
+ 
